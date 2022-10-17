293a4504f2a2ec5b33206597d32615fd08963b12
Yukti Manoj Mulani <105308291+YuktiMulani@users.noreply.github.com>
2022-10-16 20:04:30 -0400

    a-3

--- product-service.js
+++ product-service.js
@@ -1,43 +1,56 @@
-const fs = require("fs");
-
-let products = [];
-let categories = [];
-
-module.exports.initialize = function () {
+const fs = require("fs"); // required at the top of your module
+//Globally declared arrays
+var products = []
+var categories = []
+    //initialize() function that will read the content of the products.json object
+exports.initialize = () => {
     return new Promise((resolve, reject) => {
         fs.readFile('./data/products.json', 'utf8', (err, data) => {
             if (err) {
-                reject(err);
+                reject("unable to read file");
             } else {
                 products = JSON.parse(data);
-
-                fs.readFile('./data/categories.json', 'utf8', (err, data) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        categories = JSON.parse(data);
-                        resolve();
-                    }
-                });
             }
         });
-    });
-}
 
-module.exports.getAllProducts = function(){
-    return new Promise((resolve,reject)=>{
-        (products.length > 0 ) ? resolve(products) : reject("no results returned"); 
-    });
-}
+        fs.readFile('./data/categories.json', 'utf8', (err, data) => {
+            if (err) {
+                reject("unable to read file");
+            } else {
+                categories = JSON.parse(data);
+            }
+        });
+        resolve();
+    })
+};
+//function that will provide the full array of "products object"
+exports.getAllProducts = () => {
+    return new Promise((resolve, reject) => {
+        if (products.length == 0) {
+            reject('no results returned');
+        } else {
+            resolve(products);
+        }
+    })
+};
+//function that will provide an array of "product" objects whose published property is true
+exports.getPublishedProducts = () => {
+    return new Promise((resolve, reject) => {
+        var publishproducts = products.filter(product => product.published == true);
 
-module.exports.getPublishedProducts = function(){
-    return new Promise((resolve,reject)=>{
-        (products.length > 0) ? resolve(products.filter(product => product.published)) : reject("no results returned");
-    });
-}
-
-module.exports.getCategories = function(){
-    return new Promise((resolve,reject)=>{
-        (categories.length > 0 ) ? resolve(categories) : reject("no results returned"); 
-    });
-}
+        if (publishproducts.length == 0) {
+            reject('no results returned');
+        }
+        resolve(publishproducts);
+    })
+};
+//function that will provide the full array of "category" objects
+exports.getCategories = () => {
+    return new Promise((resolve, reject) => {
+        if (categories.length == 0) {
+            reject('no results returned');
+        } else {
+            resolve(categories);
+        }
+    })
+};
