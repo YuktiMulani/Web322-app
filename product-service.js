ef505e1b9b664f5b7ac36591ea07e02d100ef682
Yukti Manoj Mulani <105308291+YuktiMulani@users.noreply.github.com>
2022-11-06 23:19:15 -0500

    a4

--- product-service.js
+++ product-service.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 let products = [];
 let categories = [];
 
-module.exports.initialize = function () {
+module.exports.initialize = function() {
     return new Promise((resolve, reject) => {
         fs.readFile('./data/products.json', 'utf8', (err, data) => {
             if (err) {
@@ -24,33 +24,15 @@ module.exports.initialize = function () {
     });
 }
 
-module.exports.getAllProducts = function(){
-    return new Promise((resolve,reject)=>{
-        (products.length > 0 ) ? resolve(products) : reject("no results returned"); 
-    });
-}
-
-module.exports.getPublishedProducts = function(){
-    return new Promise((resolve,reject)=>{
-        (products.length > 0) ? resolve(products.filter(product => product.published)) : reject("no results returned");
-    });
-}
-
-module.exports.getProductsByCategory = function(category){
-    return new Promise((resolve,reject)=>{
-        let filteredproducts = products.filter(product=>product.category == category);
-
-        if(filteredproducts.length == 0){
-            reject("no results returned")
-        }else{
-            resolve(filteredproducts);
-        }
-    });
-}
-
-module.exports.getProductsByMinDate = function(minDateStr) {
+module.exports.getAllproducts = function() {
     return new Promise((resolve, reject) => {
-        let filteredproducts = products.filter(product => (new Date(product.postDate)) >= (new Date(minDateStr)))
+        (products.length > 0) ? resolve(products): reject("no results returned");
+    });
+}
+
+module.exports.getproductsByCategory = function(category) {
+    return new Promise((resolve, reject) => {
+        let filteredproducts = products.filter(product => product.category == category);
 
         if (filteredproducts.length == 0) {
             reject("no results returned")
@@ -60,34 +42,55 @@ module.exports.getProductsByMinDate = function(minDateStr) {
     });
 }
 
-module.exports.getProductById = function(id){
-    return new Promise((resolve,reject)=>{
+module.exports.getproductsByMinDate = function(minDateStr) {
+    return new Promise((resolve, reject) => {
+        let filteredproducts = products.filter(product => (new Date(product.productDate)) >= (new Date(minDateStr)))
+
+        if (filteredproducts.length == 0) {
+            reject("no results returned")
+        } else {
+            resolve(filteredproducts);
+        }
+    });
+}
+
+module.exports.getproductById = function(id) {
+    return new Promise((resolve, reject) => {
         let foundproduct = products.find(product => product.id == id);
 
-        if(foundproduct){
+        if (foundproduct) {
             resolve(foundproduct);
-        }else{
+        } else {
             reject("no result returned");
         }
     });
 }
 
-module.exports.addProduct = function(productData){
-    return new Promise((resolve,reject)=>{
-        console.log('before, productData.published',productData.published)
+module.exports.addproduct = function(productData) {
+    return new Promise((resolve, reject) => {
         productData.published = productData.published ? true : false;
-        console.log('after, productData.published',productData.published)
-        
         productData.id = products.length + 1;
-        console.log('productData.id',productData.id)
         products.push(productData);
         resolve();
     });
 }
 
+module.exports.getPublishedproducts = function() {
+    return new Promise((resolve, reject) => {
+        let filteredproducts = products.filter(product => product.published);
+        (filteredproducts.length > 0) ? resolve(filteredproducts): reject("no results returned");
+    });
+}
 
-module.exports.getCategories = function(){
-    return new Promise((resolve,reject)=>{
-        (categories.length > 0 ) ? resolve(categories) : reject("no results returned"); 
+module.exports.getCategories = function() {
+    return new Promise((resolve, reject) => {
+        (categories.length > 0) ? resolve(categories): reject("no results returned");
+    });
+}
+
+module.exports.getPublishedproductsByCategory = function(category) {
+    return new Promise((resolve, reject) => {
+        let filteredproducts = products.filter(product => product.published == true && product.category == category);
+        (filteredproducts.length > 0) ? resolve(filteredproducts): reject("no results returned");
     });
 }
--- server.js
+++ server.js
@@ -1,70 +1,173 @@
+/*********************************************************************************
+*  WEB322 – Assignment 04
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Yukti Manoj Mulani Student ID: 156809212 Date: 06-Nov-2022
+*
+*  Online (Cyclic) Link: https://dull-pear-barnacle-shoe.cyclic.app
+*
+********************************************************************************/ 
+
 const express = require('express');
-const exphbs=require('express-handlebars');
 const productData = require("./product-service");
 const multer = require("multer");
 const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
 const path = require("path");
 const app = express();
+const exphbs = require('express-handlebars');
+const stripJs = require('strip-js');
 
 const HTTP_PORT = process.env.PORT || 8080;
 
+app.engine('.hbs', exphbs.engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    helpers: {
+        navLink: function(url, options) {
+            return '<li' +
+                ((url == app.locals.activeRoute) ? ' class="active" ' : '') + '><a href="' + url + '">' + options.fn(this) + '</a></li>';
+        },
+        equal: function(lvalue, rvalue, options) {
+            if (arguments.length < 3)
+                throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
+        },
+        safeHTML: function(context) {
+            return stripJs(context);
+        }
+    }
+}));
+
+app.set('view engine', '.hbs');
+
 cloudinary.config({
-    cloud_name: 'dfigibz6e',
-    api_key: '964183147451584',
-    api_secret: 'Ap59uY4V1aViroxLP3QA6WmeqHk',
+    cloud_name: 'dpvrtu9b0',
+    api_key: '512778642227934',
+    api_secret: '8mU8y2UufWxwZQPYs7aQaODYBhI',
     secure: true
 });
 
 const upload = multer();
 
 app.use(express.static('public'));
+app.use(function(req, res, next) {
+    let route = req.baseUrl + req.path;
+    app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+    next();
+});
 
-app.engine('.hbs', exphbs.engine({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
 app.get('/', (req, res) => {
-    res.render(path.join(__dirname, "views/layouts/main.hbs"))
+    res.redirect("/product");
 });
 
-app.get('/products', (req,res)=>{
-    productData.getPublishedProducts().then((data=>{
-        res.json(data);
-    })).catch(err=>{
-        res.json({message: err});
-    });
+app.get('/home', (req, res) => {
+    res.render(path.join(__dirname + "/views/home.hbs"))
 });
 
-app.get('/demos', (req,res)=>{
+app.get('/product', async(req, res) => {
+
+    let viewData = {};
+
+    try {
+        let Products = [];
+
+        if (req.query.category) {
+            Products = await productData.getPublishedProductsByCategory(req.query.category);
+        } else {
+            Products = await productData.getPublishedProducts();
+        }
+
+        Products.sort((a, b) => new Date(b.productDate) - new Date(a.productDate));
+
+        let product = Products[0];
+
+        viewData.Products = Products;
+        viewData.product = product;
+
+    } catch (err) {
+        viewData.message = "no results";
+    }
+
+    try {
+        let categories = await productData.getCategories();
+
+        viewData.categories = categories;
+    } catch (err) {
+        viewData.categoriesMessage = "no results"
+    }
+
+    res.render("product", { data: viewData })
+
+});
+
+app.get('/product/:id', async(req, res) => {
+
+    let viewData = {};
+
+    try {
+        let Products = [];
+
+        if (req.query.category) {
+            Products = await productData.getPublishedProductsByCategory(req.query.category);
+        } else {
+            Products = await productData.getPublishedProducts();
+        }
+
+        Products.sort((a, b) => new Date(b.productDate) - new Date(a.productDate));
+
+        viewData.Products = Products;
+
+    } catch (err) {
+        viewData.message = "no results";
+    }
+
+    try {
+        viewData.product = await productData.getproductById(req.params.id);
+    } catch (err) {
+        viewData.message = "no results";
+    }
+
+    try {
+        let categories = await productData.getCategories();
+
+        viewData.categories = categories;
+    } catch (err) {
+        viewData.categoriesMessage = "no results"
+    }
+
+    res.render("product", { data: viewData })
+});
+
+app.get('/Products', (req, res) => {
 
     let queryPromise = null;
 
-    if(req.query.category){
+    if (req.query.category) {
         queryPromise = productData.getProductsByCategory(req.query.category);
-    }else if(req.query.minDate){
+    } else if (req.query.minDate) {
         queryPromise = productData.getProductsByMinDate(req.query.minDate);
-    }else{
+    } else {
         queryPromise = productData.getAllProducts()
-    } 
+    }
 
-    queryPromise.then(data=>{
-        res.json(data);
-    }).catch(err=>{
-        res.json({message: err});
+    queryPromise.then(data => {
+        res.render("Products", { Products: data });
+    }).catch(err => {
+        res.render("Products", { message: err });
     })
 
 });
 
-app.get('/categories', (req,res)=>{
-    productData.getCategories().then((data=>{
-        res.json(data);
-    })).catch(err=>{
-        res.json({message: err});
-    });
-});
+app.post("/Products/add", upload.single("featureImage"), (req, res) => {
 
-app.post("/products/add", upload.single("featureImage"), (req,res)=>{
-
-    if(req.file){
+    if (req.file) {
         let streamUpload = (req) => {
             return new Promise((resolve, reject) => {
                 let stream = cloudinary.uploader.upload_stream(
@@ -76,60 +179,64 @@ app.post("/products/add", upload.single("featureImage"), (req,res)=>{
                         }
                     }
                 );
-    
+
                 streamifier.createReadStream(req.file.buffer).pipe(stream);
             });
         };
-    
+
         async function upload(req) {
             let result = await streamUpload(req);
             console.log(result);
             return result;
         }
-    
-        upload(req).then((uploaded)=>{
-            processProduct(uploaded.url);
+
+        upload(req).then((uploaded) => {
+            processproduct(uploaded.url);
         });
-    }else{
-        processProduct("");
+    } else {
+        processproduct("");
     }
 
-    function processProduct(imageUrl){
+    function processproduct(imageUrl) {
         req.body.featureImage = imageUrl;
-
-        productData.addProduct(req.body).then(product=>{
-            res.redirect("/demos");
-        }).catch(err=>{
+        req.body.productDate = new Date().toISOString();
+        productData.addproduct(req.body).then(product => {
+            res.redirect("/Products");
+        }).catch(err => {
             res.status(500).send(err);
         })
-    }   
+    }
 });
 
-app.get('/products/add', (req,res)=>{
-   res.render(path.join(__dirname, "/views/addProduct.hbs"));
-}); 
+app.get('/Products/add', (req, res) => {
+    res.render(path.join(__dirname + "/views/addproduct.hbs"))
+});
 
-app.get('/product/:id', (req,res)=>{
-    productData.getProductById(req.params.id).then(data=>{
+app.get('/product/:id', (req, res) => {
+    productData.getproductById(req.params.id).then(data => {
         res.json(data);
-    }).catch(err=>{
-        res.json({message: err});
+    }).catch(err => {
+        res.json({ message: err });
     });
 });
 
+app.get('/categories', (req, res) => {
+    productData.getCategories().then((data => {
+        res.render("categories", { categories: data });
+    })).catch(err => {
+        res.render("categories", { message: err });
+    });
+});
 
-
-
-
-
-app.use((req,res)=>{
-    res.status(404).send("404 - Page Not Found")
+app.use((req, res) => {
+    res.status(404).render(path.join(__dirname + "/views/404.hbs"));
+    res
 })
 
-productData.initialize().then(()=>{
-    app.listen(HTTP_PORT, () => { 
-        console.log('server listening on: ' + HTTP_PORT); 
+productData.initialize().then(() => {
+    app.listen(HTTP_PORT, () => {
+        console.log('server listening on: ' + HTTP_PORT);
     });
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 })
